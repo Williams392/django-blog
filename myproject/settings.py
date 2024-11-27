7f5d6c8717c6dea84884cb538683faedbaebd325
Williams392 <williamsvalle392@gmail.com>
2024-11-26 20:34:30 -0500

    bd mysql new de google cloud a server de chile

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,11 +75,22 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bd_blog',
+        'USER': 'root',
+        'PASSWORD': 'admin123456',
+        'HOST': '34.176.101.152',
+        'PORT': '3306',
+    },
 }
 
 
